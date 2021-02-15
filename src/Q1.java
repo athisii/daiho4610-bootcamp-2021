e22b3dcd585f3efe28f8dcf63652afe3539aad8e
athisii <athisiiekhe12@gmail.com>
2021-02-15 16:50:56 +0530

    Added Q1

---
+++ src/Q1.java
@@ -0,0 +1,7 @@
+public class Q1 {
+    public static void main(String[] args) {
+        String initString = "Hello World!";
+        String replacedString = initString.replace("World", "Java");
+        System.out.println(replacedString);
+    }
+}
